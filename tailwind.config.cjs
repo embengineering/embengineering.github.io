6bb7db14cf3bd9ed67ea91dbef2104faebef326d
Emmanuel Morales <emorales0024@gmail.com>
2023-01-16 19:20:19 +0000

    Set up Tailwind CSS, Eslint, Prettier, Layout, Husky, Cspell

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "profile-mobile": "url('https://res.cloudinary.com/embengineering/image/upload/v1673893549/profile-square.png')",
+        "profile-desktop": "url('https://res.cloudinary.com/embengineering/image/upload/v1640271532/profile-pic.jpg')",
+        "screen-image": "url('https://res.cloudinary.com/embengineering/image/upload/v1673896338/IMG_20210704_182759-EFFECTS.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
